aae8a335dd4407dde20dec517cabe21e66240c7f
The Vinh <vinhnguyen@descartes.network>
2022-08-12 16:10:34 +0700

    add farm actions

--- src/view/farms/farmDetails/index.tsx
+++ src/view/farms/farmDetails/index.tsx
@@ -1,11 +1,15 @@
 import { Col, Row } from 'antd'
 import FarmInfomations from './Infomations'
+import FarmActions from './farmActions'
 
 const FarmDetails = () => {
   return (
     <Row gutter={[24, 24]}>
       <Col span={16}>
         <FarmInfomations />
+      </Col>
+      <Col span={8}>
+        <FarmActions />
       </Col>
     </Row>
   )
