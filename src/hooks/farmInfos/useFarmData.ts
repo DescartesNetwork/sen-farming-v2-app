b61b118fc3c60967fc0b2014dfaa9cf75a6a8b30
minhcong <83624415+dmcong@users.noreply.github.com>
2022-08-14 16:56:47 +0700

    fix watcher

--- src/hooks/farmInfos/useFarmData.ts
+++ src/hooks/farmInfos/useFarmData.ts
@@ -3,7 +3,5 @@ import { AppState } from 'model'
 
 export const useFarmData = (farmAddress: string) => {
   const farmData = useSelector((state: AppState) => state.farms[farmAddress])
-  console.log('farmData', farmData)
-  console.log('farmAddress', farmAddress)
   return farmData
 }
