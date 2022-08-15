ed830d18c3775fa57db0456d5b87fa23a3c0960d
minhcong <83624415+dmcong@users.noreply.github.com>
2022-08-15 12:01:02 +0700

    Update harvestAmount.tsx

--- src/actions/harvest/harvestAmount.tsx
+++ src/actions/harvest/harvestAmount.tsx
@@ -1,23 +1,27 @@
-import { useMintDecimals, util } from '@sentre/senhub'
-import { utilsBN } from '@sen-use/web3'
-
 import { Space, Typography } from 'antd'
+import MintAmount from 'components/mint/mintAmount'
+import MintTotalValue from 'components/mint/mintTotalValue'
 
-import { usePrice } from 'hooks/useGetPrice'
 import { PendingRewardData } from 'hooks/useConvertRewards'
 
 type HarvestAmountProps = { reward: PendingRewardData }
 const HarvestAmount = ({ reward }: HarvestAmountProps) => {
-  const price = usePrice(reward.mint)
-  const decimals = useMintDecimals({ mintAddress: reward.mint }) || 0
-  const mintAmount = Number(utilsBN.undecimalize(reward.amount, decimals))
-
   return (
     <Space direction="vertical" size={0} style={{ textAlign: 'right' }}>
       <Typography.Title level={5}>
-        {util.numeric(mintAmount).format('0,0.[00000]')}
+        <MintAmount
+          mintAddress={reward.mint}
+          amount={reward.amount}
+          format="0,0.[00000]"
+        />
       </Typography.Title>
-      <Typography.Text type="secondary">{123}</Typography.Text>
+      <Typography.Text type="secondary">
+        <MintTotalValue
+          mintAddress={reward.mint}
+          amount={reward.amount}
+          format="0,0.[00000]"
+        />
+      </Typography.Text>
     </Space>
   )
 }
