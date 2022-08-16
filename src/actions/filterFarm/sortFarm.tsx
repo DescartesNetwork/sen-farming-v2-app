f71375c8fbe4cc6d029516b7ddd271c9b870c574
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-08-16 14:05:35 +0700

    Format sort type

--- src/actions/filterFarm/sortFarm.tsx
+++ src/actions/filterFarm/sortFarm.tsx
@@ -4,20 +4,17 @@ import IonIcon from '@sentre/antd-ionicon'
 import { Button, Col, Divider, Row, Space, Switch, Typography } from 'antd'
 
 import { AppState } from 'model'
-import { setSort, switchBoostOnly } from 'model/main.controller'
+import { setSort, SortDirection, switchBoostOnly } from 'model/main.controller'
 
 const ICON_COLOR = {
-  ADS: { up: '#727272', down: '#F3F3F3' },
+  ASC: { up: '#727272', down: '#F3F3F3' },
   DESC: { up: '#F3F3F3', down: '#727272' },
+  null: { up: '#727272', down: '#727272' },
 }
 
-export type sortDirection = 'ADS' | 'DESC'
-
-type IconSortProps = { type?: sortDirection }
+type IconSortProps = { type: SortDirection }
 const IconSort = ({ type }: IconSortProps) => {
-  const iconColor = !type
-    ? { up: '#727272', down: '#727272' }
-    : ICON_COLOR[type]
+  const iconColor = ICON_COLOR[type]
 
   return (
     <Row style={{ flexDirection: 'column' }}>
@@ -39,7 +36,7 @@ const IconSort = ({ type }: IconSortProps) => {
 
 const SortFarm = () => {
   const dispatch = useDispatch()
-  const sort = useSelector((state: AppState) => state.main.sort)
+  const sortType = useSelector((state: AppState) => state.main.sortType)
 
   return (
     <Row gutter={[16, 16]} align="middle">
@@ -52,13 +49,21 @@ const SortFarm = () => {
               size="small"
               style={{ padding: 0, background: 'transparent' }}
               onClick={() => {
-                const direction = sort['liquidity'] === 'ADS' ? 'DESC' : 'ADS'
-                dispatch(setSort({ liquidity: direction }))
+                const direction =
+                  sortType.liquidity === SortDirection.ASC
+                    ? SortDirection.DESC
+                    : SortDirection.ASC
+                dispatch(
+                  setSort({
+                    liquidity: direction,
+                    apr: SortDirection.null,
+                  }),
+                )
               }}
             >
               Liquidity
             </Button>
-            <IconSort type={sort['liquidity']} />
+            <IconSort type={sortType.liquidity} />
           </Space>
           <Divider type="vertical" style={{ margin: 0 }} />
           <Space>
@@ -67,13 +72,18 @@ const SortFarm = () => {
               size="small"
               style={{ padding: 0, background: 'transparent' }}
               onClick={() => {
-                const direction = sort['apr'] === 'ADS' ? 'DESC' : 'ADS'
-                dispatch(setSort({ apr: direction }))
+                const direction =
+                  sortType.apr === SortDirection.ASC
+                    ? SortDirection.DESC
+                    : SortDirection.ASC
+                dispatch(
+                  setSort({ liquidity: SortDirection.null, apr: direction }),
+                )
               }}
             >
               APR
             </Button>
-            <IconSort type={sort['apr']} />
+            <IconSort type={sortType.apr} />
           </Space>
         </Space>
       </Col>
