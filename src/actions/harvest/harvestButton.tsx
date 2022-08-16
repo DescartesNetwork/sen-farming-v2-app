ead99fdd7c884e22f60451b37addeacc654e8881
minhcong <83624415+dmcong@users.noreply.github.com>
2022-08-16 18:52:26 +0700

    Update harvestButton.tsx

--- src/actions/harvest/harvestButton.tsx
+++ src/actions/harvest/harvestButton.tsx
@@ -1,26 +1,35 @@
-import { MouseEvent } from 'react'
+import { MouseEvent, useCallback, useEffect, useState } from 'react'
 
 import { Button } from 'antd'
 import { useHarvest } from 'hooks/actions/useHarvest'
 import { SizeType } from 'antd/lib/config-provider/SizeContext'
+import { useConvertRewards } from 'hooks/useConvertRewards'
 
 type HarvestButtonProps = {
   farmAddress: string
   size?: SizeType
-  disabled?: boolean
 }
 const HarvestButton = ({
   farmAddress,
   size = 'middle',
-  disabled = false,
 }: HarvestButtonProps) => {
   const { loading, harvest } = useHarvest(farmAddress)
+  const [disabled, setDisable] = useState(true)
+  const convertRewards = useConvertRewards(farmAddress)
 
   // stopPropagation() to stop propagation to parent elements.
   const onHarvest = (e: MouseEvent<HTMLElement>) => {
     e.stopPropagation()
     return harvest()
   }
+
+  const updateStatus = useCallback(() => {
+    const canHarvest = !convertRewards[0]?.amount.isZero()
+    setDisable(!canHarvest)
+  }, [convertRewards])
+  useEffect(() => {
+    updateStatus()
+  }, [updateStatus])
 
   return (
     <Button
