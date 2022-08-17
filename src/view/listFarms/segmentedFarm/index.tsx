93eaf794905f1e0362bb53ec43be9acfdde29820
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-08-17 13:50:33 +0700

    Add value control for filter set

--- src/view/listFarms/segmentedFarm/index.tsx
+++ src/view/listFarms/segmentedFarm/index.tsx
@@ -1,10 +1,12 @@
 import { Segmented } from 'antd'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { setFarmTab } from 'model/main.controller'
 import { FarmTab } from 'constant'
+import { AppState } from 'model'
 
 const SegmentedFarm = () => {
+  const farmTab = useSelector((state: AppState) => state.main.farmTab)
   const dispatch = useDispatch()
 
   return (
@@ -15,6 +17,7 @@ const SegmentedFarm = () => {
         { value: FarmTab.Your, label: FarmTab.Your },
         { value: FarmTab.Expired, label: FarmTab.Expired },
       ]}
+      value={farmTab}
       onChange={(val) => dispatch(setFarmTab(val.toString()))}
     />
   )
