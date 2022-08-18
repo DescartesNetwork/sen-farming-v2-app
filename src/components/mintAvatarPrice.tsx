e97cef3203ece68817aa2cd3d4f142add946eb47
minhcong <83624415+dmcong@users.noreply.github.com>
2022-08-18 14:19:22 +0700

    Update mintAvatarPrice.tsx

--- src/components/mintAvatarPrice.tsx
+++ src/components/mintAvatarPrice.tsx
@@ -1,7 +1,30 @@
-import { CSSProperties } from 'react'
-import { MintAvatar, MintPrice, MintSymbol } from '@sen-use/app'
+import { CSSProperties, useCallback, useEffect, useState } from 'react'
+import { Address } from '@project-serum/anchor'
+import { MintAvatar, MintSymbol, useGetMintPrice } from '@sen-use/app'
 
 import { Space, Typography } from 'antd'
+import { util } from '@sentre/senhub'
+
+const MintPrice = ({
+  mintAddress,
+  format = '$0,0.[0000]',
+}: {
+  mintAddress: Address
+  format?: string
+}) => {
+  const [price, setPrice] = useState('$0')
+  const getMintPrice = useGetMintPrice()
+
+  const updateMintPrice = useCallback(async () => {
+    const price = await getMintPrice(mintAddress)
+    return setPrice(util.numeric(price).format(format))
+  }, [format, mintAddress, getMintPrice])
+  useEffect(() => {
+    updateMintPrice()
+  }, [updateMintPrice])
+
+  return <span>{util.numeric(price).format(format)}</span>
+}
 
 type MintAvatarPriceProps = {
   mintAddress: string
