f6fc6d39793418922faefc6e52de303ab47c6472
tuanle161099 <tuanle@descartes.network>
2022-08-18 22:16:18 +0700

    fix bug

--- src/constant/farm.ts
+++ src/constant/farm.ts
@@ -9,7 +9,7 @@ export type FARM_TOKEN = {
 export const FARM_GET_TOKENS: Record<string, { [x: string]: FARM_TOKEN }> = {
   FxV5pG3H5VjLqhFxtMXJbiRZKdC7BTkv4dd2CWpVXWq8: {
     balansol: {
-      url: 'https://hub.sentre.io/app/balansol/swap?bid_mint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&ask_mint=SENBBKVCM7homnf5RX9zqpf1GFe935hnbU4uVzY1Y6M',
+      url: 'https://hub.sentre.io/app/balansol/swap?bid_mint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&ask_mint=SENBBKVCM7homnf5RX9zqpf1GFe935hnbU4uVzY1Y6M&autoInstall=true',
       description: 'Swap on Balansol',
       icon: 'https://descartesnetwork.github.io/balansol/static/asset/logo.f98afe9edf28d217.png',
     },
@@ -21,7 +21,7 @@ export const FARM_GET_TOKENS: Record<string, { [x: string]: FARM_TOKEN }> = {
   },
   D4qkfuXJUXuPUwta349c9U2ThDHnaJsEjnCAb3gLSmqi: {
     balansol: {
-      url: 'https://hub.sentre.io/app/balansol/swap?bid_mint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&ask_mint=xALGoH1zUfRmpCriy94qbfoMXHtK6NDnMKzT4Xdvgms',
+      url: 'https://hub.sentre.io/app/balansol/swap?bid_mint=EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v&ask_mint=xALGoH1zUfRmpCriy94qbfoMXHtK6NDnMKzT4Xdvgms&autoInstall=true',
       description: 'Swap on Balansol',
       icon: 'https://descartesnetwork.github.io/balansol/static/asset/logo.f98afe9edf28d217.png',
     },
